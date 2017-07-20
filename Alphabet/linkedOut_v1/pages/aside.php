402697580a1534cafe90db45201e20428d2fe306
anisElAroui <anis.el.aroui@gmail.com>
2017-07-20 16:58:54 +0200

    last update

---
+++ Alphabet/linkedOut_v1/pages/aside.php
@@ -0,0 +1,68 @@
+
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../dist/img/user1-128x128.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Moetaz Ben Charrada</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+
+
+
+
+
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Home</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> New feeds</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Settings</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Apps</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Facebook</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Twitter</a></li>
+          </ul>
+        </li>
+
+        
+        <li>
+          <a href="#">
+            <i class="fa fa-envelope"></i> <span>outMail</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>
+            </span>
+          </a>
+        </li>
+      </ul>
+
+    </section>
+    <!-- /.sidebar -->
+  </aside>
