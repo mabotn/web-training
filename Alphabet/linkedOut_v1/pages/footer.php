c7b301c0bacf88cbdd148c3af58018be0f251125
Ali Hosni <hosni.exe@gmail.com>
2017-07-20 09:18:47 +0100

    linkedOutv1

---
+++ Alphabet/linkedOut_v1/pages/footer.php
@@ -0,0 +1,35 @@
+  </section>
+    <!-- /.content -->
+    <?php
+        include('chat.php');
+?>
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <footer class="main-footer" style="margin-left:0px;">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.3.8
+    </div>
+    <strong>Copyright &copy; 2017 <a href="#">Alphabet Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<!-- Slimscroll -->
+<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+</body>
+</html>
