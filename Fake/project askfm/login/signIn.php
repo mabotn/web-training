0c476393284fbb2391bf4fe733492ea315556b86
abyr2017 <abir.barhoumi@ensi-uma.tn>
2017-07-22 14:49:34 +0100

    final f*

--- Fake/project askfm/login/signIn.php
+++ Fake/project askfm/login/signIn.php
@@ -1,5 +1,7 @@
 <?php
+
 session_start();
+
 $db= mysqli_connect('35.160.127.179:3306','fake','true7102','fake');
 
 if($db && isset($_POST['submit']))
@@ -7,9 +9,11 @@ if($db && isset($_POST['submit']))
   $result=$db->query("select * FROM asksign where mail='{$_POST['mail']}' AND pass='{$_POST['pass']}'");
   echo $db->error;
   if (mysqli_num_rows($result) >0) {
+    $row = $result->fetch_array();
+    $id = $row['id'];
     $_SESSION['signed-in'] = true;
-    $_SESSION['mail'] = $_POST['mail'];
-    header('location: page3/index.html');
+    $_SESSION['id'] = $id ;
+    header('location: ../page3/index.html');
   }
 }
 
